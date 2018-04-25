833d4890cbda3bb680a1c77dad0844ce702a8f1c
nikhil sulegaon <nikhilsulegaon@gmail.com>
2018-04-25 04:01:34 -0600

    Adding Java Kafka boilerplate

---
+++ kafka_modules/Java_kafka_boilerplate/Kafka/src/com/bigdata/Main.java
@@ -0,0 +1,59 @@
+package com.bigdata;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class Main {
+
+    public void producer() {
+
+        String topicName = "search_string";
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 10; i++)
+            producer.send(new ProducerRecord<>(topicName, Integer.toString(i), Integer.toString(i)));
+        producer.close();
+    }
+
+    public void consumer() {
+        String topicName = "tweets";
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(Collections.singletonList(topicName));
+
+        System.out.println("Subscribed to topic " + topicName);
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(10);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("value = %s\n\n\n\n", record.value());
+        }
+    }
+}

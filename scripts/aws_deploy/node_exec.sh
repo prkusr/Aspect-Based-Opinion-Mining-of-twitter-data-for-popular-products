f858628048a6d0c446734294dbd87752594559b4
shamprasad <yespee4@gmail.com>
2018-04-26 19:43:26 -0600

    Adding scripts to setup spark cluster on AWS

--- scripts/aws_deploy/node_exec.sh
+++ scripts/aws_deploy/node_exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SPARK_VERSION="2.3.0"
 IS_MASTER_NODE="f"
-java -version || sudo apt-get -y update && sudo apt-get -y install openjdk-8-jdk
+java -version || (sudo apt-get -y update && sudo apt-get -y install openjdk-8-jdk)
 
 ls ~/SparkBusters/spark-$SPARK_VERSION-bin-hadoop2.7.tgz || (mkdir -p ~/SparkBusters && cd ~/SparkBusters && wget http://apache.claz.org/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop2.7.tgz)
 ls ~/SparkBusters/spark-$SPARK_VERSION-bin-hadoop2.7 || (cd ~/SparkBusters && tar zxvf spark-$SPARK_VERSION-bin-hadoop2.7.tgz)

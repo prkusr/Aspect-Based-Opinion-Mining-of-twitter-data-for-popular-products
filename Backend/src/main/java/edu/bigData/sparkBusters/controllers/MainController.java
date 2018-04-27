dfce8b0f96be9505940ce35be00e18fbfe6fcb51
prasanna <prasanna.srinivasachar@colorado.edu>
2018-04-27 04:19:12 -0600

    Random location generator- using Streams API Java 8

--- Backend/src/main/java/edu/bigData/sparkBusters/controllers/MainController.java
+++ Backend/src/main/java/edu/bigData/sparkBusters/controllers/MainController.java
@@ -1,26 +1,29 @@
 package edu.bigData.sparkBusters.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.bigData.sparkBusters.model.GoogleMapEntry;
 
-@Controller
+@CrossOrigin(origins = "http://localhost:8000")
+@RestController
 public class MainController {
 
-	@RequestMapping("/ping")
-	public @ResponseBody List<GoogleMapEntry> getPositions() {
-		return new ArrayList<GoogleMapEntry>() {
-			private static final long serialVersionUID = 1L;
-			{
-				add(GoogleMapEntry.builder().weight(1245).lat(14.25).lng(45.4455).build());
-				add(GoogleMapEntry.builder().weight(546).lat(24.2465).lng(25.124).build());
-				add(GoogleMapEntry.builder().weight(789).lat(44.2536).lng(75.5643).build());
-			}
-		};
+	@GetMapping("/ping")
+	public @ResponseBody List<GoogleMapEntry> getPositions(@RequestParam(value = "size", defaultValue = "100") int size) {
+		// return new ArrayList<GoogleMapEntry>() {
+		// private static final long serialVersionUID = 1L;
+		// {
+		// add(GoogleMapEntry.builder().weight(1245).lat(14.25).lng(45.4455).build());
+		// add(GoogleMapEntry.builder().weight(546).lat(24.2465).lng(25.124).build());
+		// add(GoogleMapEntry.builder().weight(789).lat(44.2536).lng(75.5643).build());
+		// }
+		// };
+		return GoogleMapEntry.getRandomPoints(size);
 	}
 }

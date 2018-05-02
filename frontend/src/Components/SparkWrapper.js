5b8347bdeb3eb238ffc8d860cd1df1c2aa6e3579
prasanna <prasanna.srinivasachar@colorado.edu>
2018-05-01 19:19:55 -0600

    Update parent state from child working in SparkWrapper | Prasanna

--- frontend/src/Components/SparkWrapper.js
+++ frontend/src/Components/SparkWrapper.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import SparkAppBody from "./SparkAppBody";
+import SparkSearchBar from "./SparkSearchBar";
 import { fetchPoints } from "../RESTApi/consumer";
 // import {PieChart}  from "react-chartjs/Pie";
 import { Doughnut, Polar } from "react-chartjs-2";
@@ -11,7 +11,7 @@ import InfoSubPanel from './InfoSubPanel';
 export default class SparkWrapper extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { opinions: [] };
+		this.state = { opinions: [] ,positions : [] };
 	}
 
 	fetchPositions(q) {
@@ -21,7 +21,20 @@ export default class SparkWrapper extends Component {
 		// 	this.setState({ opinions: r });
 		// });
 		//console.log(fetchPoints(node.value))
-		return this.setState({ opinions: fetchPoints(node.value) });
+		return this.setState({ opinions: fetchPoints(q) });
+	}
+
+	updateLocation(p){
+		this.setState({ opinions: p });
+	}
+
+	updatePositions(p){
+		this.setState({ positions: p });
+	}
+
+	addToMap(m, k, v) {
+		m[k] = m[k] || [];
+		m[k].push(v);
 	}
 
 	render() {
@@ -68,8 +81,64 @@ export default class SparkWrapper extends Component {
 			{sentiment:-2,tweet:"Interesting please. Shady's back"}
 			]
 		
+		let categories = [];
 
+		for (let i = 0; i < 4 ; i++){
+			categories.push(<div className="category" value={i} key={i}>
+						Yayyy
+					</div>)
+		}
+
+		// if( )
+
+		let position = [];
+		let categoryMap = {};
+		let totalTweets = this.state.opinions.length;
+		let opinionTweets = 0;
+		let positiveOpinionTweets = 0;
+		let negativeOpinionTweets = 0;
+
+		this.state.opinions.map(o => {
+			if (o.isOpinion) {
+				position.push({
+					lat: o.location[1],
+					lng: o.location[0],
+					tweet: o.text
+				});
+
+				opinionTweets++;
+
+				o.aspects.map(aspect => {
+					// if (aspect.sentiment > 0) positiveOpinionTweets++;
+					// else if (aspect.sentiment > 0) negativeOpinionTweets++;
+					// else neutral++;
+
+					this.addToMap(categoryMap, aspect.category, {
+						tweet: o.text,
+						sentiment: aspect.sentiment,
+						lat: o.location[1],
+						lng: o.location[0]
+					});
+					return true;
+				});
+			}
+			return true;
+		});
+
+		console.log(categoryMap);	
+		console.log(position);
 		return (
+			<div id="wrap">
+			{/*<div className="MapComponent">
+          <Route
+            path="/"
+            render={props => (
+              <TweetMap google={this.props.google} positions={this.state.positions} {...props} />
+            )}
+          />
+        </div>
+      */}
+
 			<div id="app-sidebar" style={h1Style}>
 				{/*<div id="main">
 					<h1 style={h1Style}>SparkBusters</h1>
@@ -107,7 +176,7 @@ export default class SparkWrapper extends Component {
 						</div>
 					</div>
 				</div>
-				*/}
+				
 				<div id="custom-search-input" className="col-md-offset-1">
 					<div className="input-group col-md-12 ">
 						<input
@@ -127,37 +196,23 @@ export default class SparkWrapper extends Component {
 						</span>
 					</div>
 				</div>
+				*/}
 
-				<div
-					id="custom-search-input"
-					className="col-md-offset-1"
-					style={{ marginTop: "20px" }}
-				>
-					<div className="input-group col-md-12 ">
-						<input
-							ref="search"
-							type="text"
-							className="form-control input-lg"
-							placeholder="Enter a city location"
-						/>
-						<span className="input-group-btn">
-							<button
-								className="btn btn-info btn-lg"
-								type="button"
-								onClick={this.fetchPositions.bind(this)}
-							>
-								<i className="glyphicon glyphicon-search" />
-							</button>
-						</span>
-					</div>
-				</div>
-				{/*
+
+							{/*
 				<InfoSubPanel type={true} tweets={testTweet}/>
 				<InfoSubPanel type={false} tweets={tt}/>
 				*/}
-				<GraphPanel />
+				
+				<SparkSearchBar fetchPositions = {this.fetchPositions.bind(this)} />
+
+				<div className="graph-panel">
+					<h1> Opinion categories </h1>
+					{categories}
+				</div>
 				
 				{/*{body} */}
+			</div>
 			</div>
 		);
 	}
